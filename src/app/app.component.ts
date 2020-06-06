ee89abf27fdad26a5be89662b3c6a892216920c8
khyati_bardolia <khyati.bardolia@gmail.com>
2020-06-06 12:23:01 +0530

    - Minor fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,12 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor(private router: Router) {
-  }
+export class AppComponent {
 
   title = 'employee-crud-app';
-  showSideBar = false;
-
-  ngOnInit() {
-    if (this.router.url !== '/') {
-      this.showSideBar = true;
-    }
-    console.log('this.showSideBar', this.showSideBar);
-  }
 
 }
